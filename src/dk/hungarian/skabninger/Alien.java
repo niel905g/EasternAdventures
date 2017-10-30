d44d105b689f714651a7828d94034f7bac3ae5f3
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-10-30 12:48:50 +0100

    Opdateret Alien klassen med oprindelsesPlanet og en LaserPistol genstand.

--- src/dk/hungarian/skabninger/Alien.java
+++ src/dk/hungarian/skabninger/Alien.java
@@ -6,14 +6,16 @@ import dk.hungarian.Skabning;
 public class Alien extends Skabning {
 
     int alder;
+    String oprindelsesPlanet;
 
-    public Alien(String navn, String beskrivelse, int styrke, Gender gender, int alder) {
+    public Alien(String navn, String beskrivelse, int styrke, Gender gender, int alder, String oprindelsesPlanet) {
         super(navn, beskrivelse, styrke, gender);
         this.alder = alder;
+        this.oprindelsesPlanet = oprindelsesPlanet;
     }
 
     @Override
     public String toString() {
-        return "Aliens navn er " + this.getNavn() + ", den er " + this.alder + " og det er en " + this.getGender();
+        return "Aliens navn er " + this.getNavn() + ", den er " + this.alder + ", det er en " + this.getGender() + " og han kommer fra " + this.oprindelsesPlanet;
     }
 }
