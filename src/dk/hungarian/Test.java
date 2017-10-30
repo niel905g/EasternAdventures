52cb9c1ef608940db3c1ff5c44ce5be21f53e70b
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-10-30 12:54:15 +0100

    Fakkel og Vampyr

--- src/dk/hungarian/Test.java
+++ src/dk/hungarian/Test.java
@@ -1,5 +1,8 @@
 package dk.hungarian;
 
+import dk.hungarian.genstande.Fakkel;
+import dk.hungarian.skabninger.Vampyr;
+
 public class Test {
 
     public static void test1() {
@@ -10,7 +13,16 @@ public class Test {
 
         Skabning s = new Skabning("as", "as", 1, Gender.APACHE_HELICOPTER);
         // Man kan ikke instanciere en abstract klasse
-        // Genstand g = new Genstand();
+        // Genstand g =
+
+
+        Fakkel fakkel1 = new Fakkel(376);
+        System.out.println(fakkel1.toString());
+
+        System.out.println("");
+
+        Vampyr vampyr1 = new Vampyr(23);
+        System.out.println(vampyr1.toString());
     }
 
 }
