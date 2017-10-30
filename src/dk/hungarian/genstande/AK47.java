83e7c88a5956f071873ef477d6e4c79811f4ec25
Andras Acs <acsandras@gmail.com>
2017-10-30 12:36:43 +0100

    Har ændret Genstand til abstract.

---
+++ src/dk/hungarian/genstande/AK47.java
@@ -0,0 +1,10 @@
+package dk.hungarian.genstande;
+
+import dk.hungarian.Genstand;
+
+public class AK47 extends Genstand {
+
+    public AK47(String navn, String beskrivelse, int styrke) {
+        super(navn, beskrivelse, styrke);
+    }
+}
