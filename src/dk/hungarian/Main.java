7f99a7328e975bc7eb9cc447cbdae6cb6d48c5c1
Johan <Joha6163@edu.easj.dk>
2017-10-30 13:03:40 +0100

    Tilføjet genstand og skabning

--- src/dk/hungarian/Main.java
+++ src/dk/hungarian/Main.java
@@ -25,24 +25,5 @@ public class Main {
             Player player = new Player(navn); }
         Test.test1();
 
-
-
-
-
-
-
-
-
-
-
-
-
-        /*Constructors til tingene
-        SurMor surmor = new SurMor(25);
-        System.out.println("Skabning " + surmor.getNavn() + "(" + surmor.getBeskrivelse() + ") har en surhedsgrad på: " + surmor.getStyrke() + ".");
-        FluffyBunny_sværd fluffybunny = new FluffyBunny_sværd();
-        System.out.println("Sværd " + fluffybunny.getNavn() + "(" + fluffybunny.getBeskrivelse() + "). Du laver et par slag ud i luften.." + fluffybunny.getLyd() + ". Du kan mærke kraften fra sværdet gennemstrømme din krop.");
-
-*/
     }
 }
