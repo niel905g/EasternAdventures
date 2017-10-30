a017d3a9b089cd47998b2d664c84415700126a1b
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2017-10-30 11:37:58 +0100

    Helle Thorning boss added

---
+++ src/dk/hungarian/skabninger/Helle_Thorning.java
@@ -0,0 +1,13 @@
+package dk.hungarian.skabninger;
+
+import dk.hungarian.Gender;
+import dk.hungarian.Skabning;
+
+public class Helle_Thorning extends Skabning {
+    
+
+    public Helle_Thorning(String navn, String beskrivelse, int styrke, Gender gender) {
+        super(" Helle Thorning ", " Wild Feminist ", 4, Gender.APACHE_HELICOPTER);
+    }
+
+}
