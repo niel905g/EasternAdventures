0636e9d62d72a6fa05b7244441472e161d85bcbf
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2017-10-30 11:17:55 +0100

    Helle Thorning boss added

--- src/dk/hungarian/genstande/Default.java
+++ src/dk/hungarian/genstande/Default.java
@@ -1,4 +1,8 @@
 package dk.hungarian.genstande;
 
+import dk.hungarian.Gender;
+import dk.hungarian.Skabning;
+
 public class Default {
+    Skabning level4Feminist = new Skabning(" Helle Thorning", " You have triggered a wild feminist ", 2, Gender.APACHE_HELICOPTER);
 }
