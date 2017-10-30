98f00a64cc83cec4851e52c0ae776d2415e0929d
Johan <Joha6163@edu.easj.dk>
2017-10-30 13:01:58 +0100

    Tilføjet genstand og skabning

--- src/dk/hungarian/Main.java
+++ src/dk/hungarian/Main.java
@@ -3,6 +3,9 @@
  */
 package dk.hungarian;
 
+import dk.hungarian.genstande.FluffyBunny_sværd;
+import dk.hungarian.skabninger.SurMor;
+
 import java.util.Scanner;
 
 public class Main {
@@ -21,5 +24,25 @@ public class Main {
             String navn = input.next();
             Player player = new Player(navn); }
         Test.test1();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Constructors til tingene
+        SurMor surmor = new SurMor(25);
+        System.out.println("Skabning " + surmor.getNavn() + "(" + surmor.getBeskrivelse() + ") har en surhedsgrad på: " + surmor.getStyrke() + ".");
+        FluffyBunny_sværd fluffybunny = new FluffyBunny_sværd();
+        System.out.println("Sværd " + fluffybunny.getNavn() + "(" + fluffybunny.getBeskrivelse() + "). Du laver et par slag ud i luften.." + fluffybunny.getLyd() + ". Du kan mærke kraften fra sværdet gennemstrømme din krop.");
+
+*/
     }
 }
